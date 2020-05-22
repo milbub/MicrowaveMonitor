058aeb1f7715d1534dc50a099bf9c11f6b9a5a60
Milan Bubniak <xbubni03@vutbr.cz>
2020-05-22 22:08:15 +0200

    Added DSPLib, libs reorganize, started working on Periodicity analyse.

---
+++ Analysers/PeriodicityAnalyser.cs
@@ -0,0 +1,67 @@
+﻿using Itenso.TimePeriod;
+using MicrowaveMonitor.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.PeerToPeer.Collaboration;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vibrant.InfluxDB.Client;
+using Vibrant.InfluxDB.Client.Rows;
+
+namespace MicrowaveMonitor.Analysers
+{
+    public class PeriodicityAnalyser : Analyser
+    {
+        private static TimeSpan CalcTenMinInterval = TimeSpan.FromMinutes(10);
+        private static TimeSpan CalcHourInterval = TimeSpan.FromMinutes(60);
+        private static TimeSpan CalcDayInterval = TimeSpan.FromDays(1);
+        private static TimeSpan CalcWeekInterval = TimeSpan.FromDays(7);
+
+        public PeriodicityAnalyser(AlarmManager alarmManager, DataManager dataManager) : base(alarmManager, dataManager)
+        {
+            RefreshInterval = CalcTenMinInterval;
+        }
+
+        public void LoadSettings(PercentDiff percentages)
+        {
+            Percentages = percentages;
+        }
+
+        protected override void Start()
+        {
+            tQueryer = new Thread(Run) { IsBackground = true, Name = "analyserPeriodicity" };
+            tQueryer.Start();
+        }
+
+        private void Run()
+        {
+            while (IsRunning)
+            {
+                DateTime start = DateTime.Now;
+
+
+
+                TimeSpan diff = DateTime.Now - start;
+                Thread.Sleep(RefreshInterval - diff);
+            }
+        }
+
+        private async void Compare(string meas, Dictionary<int, bool> watchInfo, Dictionary<int, int> ids, TimeSpan queryTimeSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeviceStopped(int devId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
